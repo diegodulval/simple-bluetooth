226d1454e21de6b9db20863af1c77b04d3101eef
Dulval <dulval@Mac-mini-de-Rafael.local>
2018-11-08 19:37:31 -0200

    first commit

---
+++ app/src/main/java/demo/dulval/com/simplebluetooth/MainActivity.java
@@ -0,0 +1,13 @@
+package demo.dulval.com.simplebluetooth;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
