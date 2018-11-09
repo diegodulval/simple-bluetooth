7792a716d658ffd233d11b85a58412de05930271
Dulval <dulval@Mac-mini-de-Rafael.local>
2018-11-09 19:32:02 -0200

    Create first connection

--- app/src/main/java/demo/dulval/com/simplebluetooth/MainActivity.java
+++ app/src/main/java/demo/dulval/com/simplebluetooth/MainActivity.java
@@ -1,13 +1,27 @@
 package demo.dulval.com.simplebluetooth;
 
+import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    int ATIVA_BLUE = 1;
+    BluetoothAdapter meuBlue = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        meuBlue = BluetoothAdapter.getDefaultAdapter();
+        if(meuBlue == null){
+            Toast.makeText(getApplicationContext(), "Seu dispositivo nao possui bluetooth", Toast.LENGTH_LONG).show();;
+        }else if(!meuBlue.isEnabled()){
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, ATIVA_BLUE);
+        }
     }
 }
