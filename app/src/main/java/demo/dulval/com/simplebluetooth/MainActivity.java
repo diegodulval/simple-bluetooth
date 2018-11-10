5ec063fc4cfbdd257a0a27f63191b051aface0b0
Dulval <dulval@Mac-mini-de-Rafael.local>
2018-11-09 22:16:28 -0200

    Create socket connection

--- app/src/main/java/demo/dulval/com/simplebluetooth/MainActivity.java
+++ app/src/main/java/demo/dulval/com/simplebluetooth/MainActivity.java
@@ -2,6 +2,8 @@ package demo.dulval.com.simplebluetooth;
 
 import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
 import android.content.Intent;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
@@ -10,6 +12,10 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import java.io.IOError;
+import java.io.IOException;
+import java.util.UUID;
+
 public class MainActivity extends AppCompatActivity {
 
     //inputs
@@ -17,10 +23,17 @@ public class MainActivity extends AppCompatActivity {
 
     private static final int SOLICITA_ATIVACAO_BLUE = 1;
     private static final int SOLICITA_CONEXAO_BLUE = 2;
+
     BluetoothAdapter meuBlue = null;
+    BluetoothDevice meuDevice = null;
+    BluetoothSocket meuSocket = null;
+
     boolean conexao = false;
 
     private static String MAC =null;
+
+    // Default UUID
+    private UUID DEFAULT_UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,6 +58,14 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 if(conexao){
                     //desconect
+                    try{
+                        meuSocket.close();
+                        Toast.makeText(getApplicationContext(), "Dispositivo desconectado!", Toast.LENGTH_LONG).show();;
+                        conexao = false;
+                        btnConexao.setText("CONECTAR");
+                    }catch(IOException erro){
+                        Toast.makeText(getApplicationContext(), "Erro ao desconectar!", Toast.LENGTH_LONG).show();;
+                    }
                 }else {
                     //connect
                     Intent abreLista = new Intent(MainActivity.this, ListaDispositivos.class);
@@ -70,6 +91,19 @@ public class MainActivity extends AppCompatActivity {
              case SOLICITA_CONEXAO_BLUE:
                  if(resultCode == Activity.RESULT_OK){
                      MAC = data.getExtras().getString(ListaDispositivos.ENDERECO_MAC);
+                     meuDevice = meuBlue.getRemoteDevice(MAC);
+
+                     try{
+                         meuSocket = meuDevice.createRfcommSocketToServiceRecord(DEFAULT_UUID);
+                         meuSocket.connect();
+                         conexao = true;
+                         btnConexao.setText("DESCONECTAR");
+                         Toast.makeText(getApplicationContext(), "Dispositivo conectado!", Toast.LENGTH_LONG).show();;
+                     }catch(IOException erro){
+                         conexao = false;
+                         Toast.makeText(getApplicationContext(), "Ocorreu um erro ao tentar conectar com o dispositivo selecionado!", Toast.LENGTH_LONG).show();;
+                     }
+
                  }else {
                      Toast.makeText(getApplicationContext(), "Falha ao obter o MAC", Toast.LENGTH_LONG).show();;
                  }
